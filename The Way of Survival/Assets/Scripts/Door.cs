a4402046688bc179a1c5d77431355e5cd8b01ae7
Skrion305 <kirasir2006@gmail.com>
2025-04-08 22:46:53 +0300

    1

--- The Way of Survival/Assets/Scripts/Door.cs
+++ The Way of Survival/Assets/Scripts/Door.cs
@@ -5,14 +5,14 @@ public class Door : MonoBehaviour
     [SerializeField] GameObject door;
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Door"))
+        if (other.CompareTag("Door"))
         {
             door.transform.rotation = Quaternion.Euler(0f, -10.31f, 0f);
         }
     }
     private void OnTriggerExit(Collider other)
     {
-        if (other.gameObject.CompareTag("Door"))
+        if (other.CompareTag("Door"))
         {
             door.transform.rotation = Quaternion.Euler(0f, 90f, 0f);
         }
