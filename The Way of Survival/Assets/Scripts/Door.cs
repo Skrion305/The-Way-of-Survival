eeb4148ead9ee522e68fbb8f5cbb710e496a1fd6
Skrion305 <kirasir2006@gmail.com>
2025-04-11 03:57:24 +0300

    Collisions

--- The Way of Survival/Assets/Scripts/Door.cs
+++ The Way of Survival/Assets/Scripts/Door.cs
@@ -5,16 +5,18 @@ public class Door : MonoBehaviour
     [SerializeField] GameObject door;
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Door"))
+        if (other.CompareTag("Player"))
         {
-            door.transform.rotation = Quaternion.Euler(0f, -10.31f, 0f);
+            Debug.Log("Da");
+            door.transform.rotation = Quaternion.Lerp(door.transform.rotation, Quaternion.Euler(0, -90f, 0), 2 * Time.deltaTime);
         }
     }
     private void OnTriggerExit(Collider other)
     {
-        if (other.CompareTag("Door"))
+        if (other.CompareTag("Player"))
         {
-            door.transform.rotation = Quaternion.Euler(0f, 90f, 0f);
+            Debug.Log("Net");
+            door.transform.rotation = Quaternion.Lerp(door.transform.rotation, Quaternion.Euler(0, 90f, 0), 2 * Time.deltaTime);
         }
     }
 }
--- The Way of Survival/Assets/Scripts/Wall.cs
+++ The Way of Survival/Assets/Scripts/Wall.cs
@@ -5,9 +5,18 @@ public class Wall : MonoBehaviour
     [SerializeField] GameObject panel;
     private void OnCollisionEnter(Collision col)
     {
-        if (col.gameObject.CompareTag("Wall"))
+        if (col.gameObject.CompareTag("Player"))
         {
+            Debug.Log("Da");
             panel.SetActive(true);
+        }
+    }
+    private void OnCollisionExit(Collision col)
+    {
+        if (col.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Net");
+            panel.SetActive(false);
         }
     }
 }
