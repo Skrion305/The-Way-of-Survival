4d69a652c4b8c515cb3a0a2ee0dfa6d157a41ab9
KirillPas <kirasir2006@gmail.com>
2025-05-05 17:09:23 +0300

    1

--- The Way of Survival/Assets/Scripts/Enemy/damagedetector.cs
+++ The Way of Survival/Assets/Scripts/Enemy/damagedetector.cs
@@ -5,9 +5,14 @@ public class Damagedetector : MonoBehaviour
 {
     int health = 100;
     [SerializeField] TMP_Text text;
+    [SerializeField] GameObject panel;
     public void ondamagedetected(int damage)
     {
         health -= damage;
         text.text = "Health: " + health.ToString();
+        if (health <= 0)
+        {
+            panel.SetActive(true);
+        }
     }
 }
