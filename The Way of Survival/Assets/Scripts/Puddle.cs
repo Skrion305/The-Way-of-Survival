3ea1920f22970c3e0ef258116859e095c26adc40
Artorm <149932990+Dumpling-OLW@users.noreply.github.com>
2025-05-27 20:21:12 +0300

    1

--- The Way of Survival/Assets/Scripts/Puddle.cs
+++ The Way of Survival/Assets/Scripts/Puddle.cs
@@ -4,12 +4,17 @@ using static UnityEngine.Rendering.GPUSort;
 public class Puddle : MonoBehaviour
 {
     bool weapon = false;
+    [SerializeField] Player player;
     void OnTriggerEnter(Collider col)
     {
         weapon = (col.CompareTag("Gun")) || (col.CompareTag("Axe")) || (col.CompareTag("Knife"));
         if (weapon)
         {
             col.gameObject.tag = "Infected";
+        }
+        if (col.CompareTag("Player"))
+        {
+            player.health = 0;
         }
     }
 }
